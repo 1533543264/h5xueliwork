4cc909fcff4152e6bbe2376742397e938cef1873
1533543264 <37681926+1533543264@users.noreply.github.com>
2019-10-27 06:29:08 -0500

    Add files via upload

---
+++ react/build/precache-manifest.d379096f84a5da71ca742bb39feb4f86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0364e72cce48b8aab8a310d7196da78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3181e03c833bf932ca7",
+    "url": "/static/css/main.394548b6.chunk.css"
+  },
+  {
+    "revision": "36dd16e37ba4d16f62f6",
+    "url": "/static/js/2.ec4fe935.chunk.js"
+  },
+  {
+    "revision": "e3181e03c833bf932ca7",
+    "url": "/static/js/main.4881bf5f.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
